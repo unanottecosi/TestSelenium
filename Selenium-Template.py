7289bd14af1896acad169adfccc43ceea2db2790
unanottecosi <30680914+unanottecosi@users.noreply.github.com>
2023-12-26 10:31:08 -0800

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -36,5 +36,5 @@ driver = webdriver.Chrome(options = chrome_options)
 driver.get('http://github.com')
 print(driver.title)
 with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+    f.write(f"This is title of GitHub home page {driver.title}")
 
